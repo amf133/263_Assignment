69bb9f2abf314e653abc3456dc01d30002c7432d
alecfox <alecfo789@gmail.com>
2020-09-03 12:43:01 +1200

    new2

--- HomePage.php
+++ HomePage.php
@@ -1,4 +1,13 @@
-<?php
-    echo "hello";
-    echo "Yooo";
-?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+    <h1>Hello</h1>
+
+    <?php
+        echo "up2";
+    ?>
+</body>
+</html>
