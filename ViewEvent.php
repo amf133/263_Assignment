1bc86b689f947da7e7ca09cc9791cc15551eb74b
amf133 <64897924+amf133@users.noreply.github.com>
2020-09-18 12:18:47 +1200

    Edited layout
    Put everything into rows/cols, fixed the back window button now working, removed some unnecessary code (still more to do)

--- ViewEvent.php
+++ ViewEvent.php
@@ -1,28 +1,32 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.10.2/css/all.css">
+    <link rel="stylesheet" href="styles.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>UC Exam Scheduler</title>
 </head>
 <body>
-<div id="viewPage">
-    <h2> Upcoming events </h2>
-    <form action="ViewEvent.php" method="post">
-        <div class="container">
-
-            <input type="submit" value="Back" onClick="myFunction()"/>
-            <script>
-                function myFunction() {
-                    window.location.href="index.php";
-                }
-            </script>
-            <br/> <br/>
+<div id="viewPage class = "container">
+    <div class = "row mt-3">
+        <div class = "col text-center">
+            <h2> Upcoming events </h2>
         </div>
-
-    </form>
-
+    </div>
+    <div class = "row mt-3">
+        <div class = "col text-center">
+            <button onClick="window.location.href='index.php'"/>Back</button>
+        </div>
+    </div>
+    <br/> <br/>
 </div>
 
 </body>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </html>
 
 <?php
--- index.php
+++ index.php
@@ -1,40 +1,68 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.10.2/css/all.css">
+    <link rel="stylesheet" href="styles.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>UC Exam Scheduler</title>
 </head>
 <body>
-    <div id="loginPage">
-        <h2> Please login </h2>
-        <form action="index.php" method="post">
-            <div class="container">
-                <label for="username"><b>Username</b></label>
-                <br/>
-                <input type="text" placeholder="Enter Username" name="user_name" required>
-                <br/> <br/>
-                <label for="psw"><b>Password</b></label>
-                <br/>
-                <input type="password" placeholder="Enter Password" name="user_password" required>
-                <br/> <br/>
-
-                <button type="submit">Login</button>
+<form method="post">
+    <div id="loginPage" class = "container">
+        <div class = "row mt-3">
+            <div class = "col text-center">
+                <h2> Please login </h2>
             </div>
-
-        </form>
-
+        </div>
+        <div class = "row align-items-center mt-3">
+            <div class = "col text-center">
+                <input type="text" placeholder="Enter Username" name="user_name" required>
+            </div>
+        </div>
+        <div class = "row mt-3">
+            <div class = "col text-center">
+                <input type="password" placeholder="Enter Password" name="user_password" required>
+            </div>
+        </div>
+        <div class = "row mt-3">
+            <div class = "col text-center">
+                <button>Login</button>
+            </div>
+        </div>
     </div>
 
-    <div id="mainPage">
-        <h2> Home Page </h2>
-        <input type="submit" value="View Events" onClick="myFunction()"/>
-        <script>
-            function myFunction() {
-                window.location.href="ViewEvent.php";
-            }
-        </script>
+</form>
+
+    <div id="mainPage" class = "container">
+        <div class = "row mt-3">
+            <div class = "col text-center">
+                <h2>Your dashboard</h2>
+            </div>
+        </div>
+        <div class = "row mt-3">
+            <div class = "col-4 text-center">
+                <h3>Welcome back {name}</h3> <!-- need to change name to cookie name or whatever -->
+            </div>
+            <div class = "col-4 text-center">
+                <button/>Create new event</button>
+            </div>
+            <div class = "col-4 text-center">
+                <button onClick="window.location.href='ViewEvent.php'"/>View events</button>
+            </div>
+        </div>
+        <div class = "row mt-3">
+            <div class = "col text-center">
+                <textarea style="min-width: 60%" placeholder="Search for new event" rows="1"></textarea>
+            </div>
+        </div>
     </div>
 
 </body>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </html>
 
 <?php
