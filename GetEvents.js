8e3dd4775d9619942927d95dda2c532d8ecf6ca4
Simon Lorimer <bumblesimsim@gmail.com>
2020-09-23 15:06:58 +1200

    Implemented Live Search
    Live search is working well, however needs a lot of styling/change of information displayed to fit design specification

--- GetEvents.js
+++ GetEvents.js
@@ -1,12 +1,35 @@
+let searchData; //info pulled from GetEvents.php and added in JSON format here
+
 $.get('GetEvents.php', function (data) {
-    var dataObj = JSON.parse(data);
-    console.log(dataObj);
-
-    var div = document.getElementById('events');
-
-    for (var i = 0; i < dataObj.length; i++) {
-        let each = JSON.parse(dataObj[i]);
-        div.innerHTML += each.event_name + ' ' + each.cluster_name + ' ' + each.date + ' ' + each.time_offset + ' ' + each.machine_group + ' ' + each.activate + '<br/>';
-        console.log(dataObj[i]);
-    }
+    searchData = JSON.parse(data);
+    showResult("");
+    return;
 });
+
+function showResult(str) {
+    let div = document.getElementById('events');
+    div.innerHTML = "<tr><th>Event Name</th><th>Date</th><th>Cluster</th><th>Offset</th><th>Group</th><th>Activate</th><th>Details</th></tr>";
+
+    if (str.length === 0) {
+        //show all results when nothing in search
+        for (let i = 0; i < searchData.length; i++) {
+            let each = JSON.parse(searchData[i]);
+            div.innerHTML += "<tr><td>" + each.event_name + "</td><td>" + each.date + "</td><td>" + each.cluster_name + "</td><td>" + each.time_offset + "</td><td>" + each.machine_group + "</td><td>" + each.activate + "</td><td><button>View Details</button></td>" + "</tr>";
+        }
+    } else if (str.length >= 3) {
+        //show results based off search based off event name
+        for (let i = 0; i < searchData.length; i++) {
+            let each = JSON.parse(searchData[i]);
+            if (((each.event_name).toLowerCase()).includes(str.toLowerCase())) {
+                div.innerHTML += "<tr><td>" + each.event_name + "</td><td>" + each.date + "</td><td>" + each.cluster_name + "</td><td>" + each.time_offset + "</td><td>" + each.machine_group + "</td><td>" + each.activate + "</td><td><button>View Details</button></td>" + "</tr>";
+            }
+        }
+    }
+    return;
+}
+
+
+
+
+
+
